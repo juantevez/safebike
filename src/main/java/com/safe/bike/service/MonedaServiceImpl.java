ec5f2f2caa18a26e95fb0d881ba9e466a07efe34
Juan Tevez <juan.tevez@gmail.com>
2025-09-21 14:04:36 -0300

    Generacion de pdf

--- src/main/java/com/safe/bike/service/MonedaServiceImpl.java
+++ src/main/java/com/safe/bike/service/MonedaServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -30,11 +32,17 @@ public class MonedaServiceImpl implements MonedaServicePort {
 
         try {
             List<MonedaEntity> monedas = monedaRepositoryPort.findAll();
-            logger.info("Se encontraron {} monedas: ", monedas.size());
-            logger.debug("Tipos de monedas obtenidas: {}", monedas);
-            return monedas;
+
+            // Ordenar alfabéticamente (asumiendo que tienes un campo 'nombre' o 'codigo')
+            List<MonedaEntity> monedasOrdenadas = monedas.stream()
+                    .sorted(Comparator.comparing(MonedaEntity::getCodigoMoneda))
+                    .collect(Collectors.toList());
+
+            logger.info("Se encontraron {} monedas ordenadas alfabéticamente", monedasOrdenadas.size());
+            logger.debug("Tipos de monedas obtenidas: {}", monedasOrdenadas);
+            return monedasOrdenadas;
         } catch (Exception e) {
-            logger.error("Error al obtener todos los tipos de bicicletas", e);
+            logger.error("Error al obtener todos los tipos de monedas", e);
             throw e;
         }
     }
